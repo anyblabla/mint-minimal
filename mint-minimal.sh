e9289426429ca201292adb70b27ddd63c6d80510
anyblabla <blablalinux@outlook.be>
2025-10-31 23:11:50 +0100

    Réorganiser et simplifier le script pour créer une installation minimale de Linux Mint Cinnamon 21 en purgant les paquets par défaut et en ajoutant des commentaires explicatifs.

--- mint-minimal.sh
+++ mint-minimal.sh
@@ -1,348 +1,122 @@
 #!/bin/bash
-#
-# Autheur:
-#   Amaury Libert <amaury-libert@hotmail.com> de Blabla Linux <https://blablalinux.be>
-#
-# Description:
-#   Script qui va supprimer une série de paquets afin d'obtenir une Linux Mint Cinnamon 21 minimal.
-#   Script that will remove a series of packages in order to obtain a minimal Linux Mint Cinnamon 21.
-#
-# Préambule Légal:
-# 	Ce script est un logiciel libre.
-# 	Vous pouvez le redistribuer et / ou le modifier selon les termes de la licence publique générale GNU telle que publiée par la Free Software Foundation; version 3.
-#
-# 	Ce script est distribué dans l'espoir qu'il sera utile, mais SANS AUCUNE GARANTIE; sans même la garantie implicite de QUALITÉ MARCHANDE ou d'ADÉQUATION À UN USAGE PARTICULIER.
-# 	Voir la licence publique générale GNU pour plus de détails.
-#
-# 	Licence publique générale GNU : <https://www.gnu.org/licenses/gpl-2.0.txt>
-#
-echo "Effacement écran..."
-clear
-#
-echo "Suppression des paquets..." 
-apt purge --ignore-missing -y \
-baobab \
-bison \
-celluloid \
-dc \
-drawing \
-fonts-opensymbol \
-gir1.2-rb-3.0 \
-gir1.2-xviewer-3.0 \
-gnome-calculator \
-gnome-calendar \
-gnome-disk-utility \
-gnome-font-viewer \
-gnome-screenshot \
-gucharmap \
-hexchat \
-hunspell \
-hunspell-af \
-hunspell-an \
-hunspell-ar \
-hunspell-be \
-hunspell-bg \
-hunspell-bn \
-hunspell-bo \
-hunspell-br \
-hunspell-bs \
-hunspell-ca \
-hunspell-cs \
-hunspell-da \
-hunspell-de-at \
-hunspell-de-at-frami \
-hunspell-de-at-frami \
-hunspell-de-at-frami \
-hunspell-de-at-frami \
-hunspell-de-ch \
-hunspell-de-ch-frami \
-hunspell-de-ch-frami \
-hunspell-de-ch-frami \
-hunspell-de-ch-frami \
-hunspell-de-de \
-hunspell-de-de-frami \
-hunspell-de-de-frami \
-hunspell-de-de-frami \
-hunspell-de-de-frami \
-hunspell-de-med \
-hunspell-dz \
-hunspell-el \
-hunspell-en-au \
-hunspell-en-au \
-hunspell-en-au \
-hunspell-en-ca \
-hunspell-en-ca \
-hunspell-en-ca \
-hunspell-en-gb \
-hunspell-en-gb \
-hunspell-en-gb \
-hunspell-en-med \
-hunspell-en-us \
-hunspell-en-us \
-hunspell-en-za \
-hunspell-en-za \
-hunspell-en-za \
-hunspell-es \
-hunspell-es \
-hunspell-es \
-hunspell-eu \
-hunspell-fr \
-hunspell-fr \
-hunspell-fr-classical \
-hunspell-fr-classical \
-hunspell-fr-classical \
-hunspell-fr-comprehensive \
-hunspell-gd \
-hunspell-gl \
-hunspell-gl-es \
-hunspell-gu \
-hunspell-gug \
-hunspell-he \
-hunspell-hi \
-hunspell-hr \
-hunspell-hu \
-hunspell-id \
-hunspell-is \
-hunspell-it \
-hunspell-it \
-hunspell-it \
-hunspell-kk \
-hunspell-kmr \
-hunspell-ko \
-hunspell-lo \
-hunspell-lt \
-hunspell-lv \
-hunspell-ml \
-hunspell-mn \
-hunspell-ne \
-hunspell-nl \
-hunspell-no \
-hunspell-oc \
-hunspell-pl \
-hunspell-pt-br \
-hunspell-pt-br \
-hunspell-pt-br \
-hunspell-pt-pt \
-hunspell-pt-pt \
-hunspell-pt-pt \
-hunspell-ro \
-hunspell-ru \
-hunspell-ru \
-hunspell-ru \
-hunspell-si \
-hunspell-sk \
-hunspell-sl \
-hunspell-sr \
-hunspell-sv \
-hunspell-sv-se \
-hunspell-sw \
-hunspell-te \
-hunspell-th \
-hunspell-tools \
-hunspell-tr \
-hunspell-uk \
-hunspell-uz \
-hunspell-vi \
-hyphen-af \
-hyphen-as \
-hyphen-bg \
-hyphen-bn \
-hyphen-ca \
-hyphen-cs \
-hyphen-da \
-hyphen-de \
-hyphen-de \
-hyphen-de \
-hyphen-el \
-hyphen-en-ca \
-hyphen-en-ca \
-hyphen-en-gb \
-hyphen-en-gb \
-hyphen-en-gb \
-hyphen-en-us \
-hyphen-en-us \
-hyphen-en-us \
-hyphen-es \
-hyphen-fi \
-hyphen-fr \
-hyphen-fr \
-hyphen-fr \
-hyphen-ga \
-hyphen-gl \
-hyphen-gu \
-hyphen-hi \
-hyphen-hr \
-hyphen-hr \
-hyphen-hu \
-hyphen-id \
-hyphen-is \
-hyphen-it \
-hyphen-it \
-hyphen-it \
-hyphen-kn \
-hyphen-lt \
-hyphen-lv \
-hyphen-ml \
-hyphen-mn \
-hyphen-mr \
-hyphen-nl \
-hyphen-no \
-hyphen-or \
-hyphen-pa \
-hyphen-pl \
-hyphen-pl \
-hyphen-pt-br \
-hyphen-pt-br \
-hyphen-pt-br \
-hyphen-pt-pt \
-hyphen-pt-pt \
-hyphen-pt-pt \
-hyphen-ro \
-hyphen-ru \
-hyphen-ru \
-hyphen-ru \
-hyphen-show \
-hyphen-sk \
-hyphen-sl \
-hyphen-sr \
-hyphen-sv \
-hyphen-ta \
-hyphen-te \
-hyphen-uk \
-hyphen-zu \
-hypnotix \
-liblibreoffice-java \
-libmythes-1.2-0 \
-libreoffice-base \
-libreoffice-base-core \
-libreoffice-base-drivers \
-libreoffice-calc \
-libreoffice-common \
-libreoffice-core \
-libreoffice-draw \
-libreoffice-gnome \
-libreoffice-gtk3 \
-libreoffice-help-common \
-libreoffice-help-de \
-libreoffice-help-de \
-libreoffice-help-en-gb \
-libreoffice-help-en-gb \
-libreoffice-help-en-us \
-libreoffice-help-en-us \
-libreoffice-help-es \
-libreoffice-help-es \
-libreoffice-help-fr \
-libreoffice-help-fr \
-libreoffice-help-it \
-libreoffice-help-it \
-libreoffice-help-pt \
-libreoffice-help-pt \
-libreoffice-help-pt-br \
-libreoffice-help-pt-br \
-libreoffice-help-ru \
-libreoffice-help-ru \
-libreoffice-help-zh-cn \
-libreoffice-help-zh-cn \
-libreoffice-help-zh-tw \
-libreoffice-help-zh-tw \
-libreoffice-impress \
-libreoffice-java-common \
-libreoffice-l10n-de \
-libreoffice-l10n-de \
-libreoffice-l10n-en-gb \
-libreoffice-l10n-en-gb \
-libreoffice-l10n-en-za \
-libreoffice-l10n-en-za \
-libreoffice-l10n-es \
-libreoffice-l10n-es \
-libreoffice-l10n-fr \
-libreoffice-l10n-fr \
-libreoffice-l10n-it \
-libreoffice-l10n-it \
-libreoffice-l10n-pt \
-libreoffice-l10n-pt \
-libreoffice-l10n-pt-br \
-libreoffice-l10n-pt-br \
-libreoffice-l10n-ru \
-libreoffice-l10n-ru \
-libreoffice-l10n-zh-cn \
-libreoffice-l10n-zh-cn \
-libreoffice-l10n-zh-tw \
-libreoffice-l10n-zh-tw \
-libreoffice-math \
-libreoffice-sdbc-hsqldb \
-libreoffice-style-colibre \
-libreoffice-writer \
-librhythmbox-core10 \
-libsort-versions-perl \
-libuno-cppu3 \
-libuno-cppuhelpergcc3-3 \
-libunoloader-java \
-libuno-purpenvhelpergcc3-3 \
-libuno-sal3 \
-libuno-salhelpergcc3-3 \
-mintstick \
-mythes-de \
-mythes-de \
-mythes-de-ch \
-mythes-de-ch \
-mythes-en-au \
-mythes-en-us \
-mythes-en-us \
-mythes-fr \
-mythes-fr \
-mythes-it \
-mythes-it \
-mythes-pt-pt \
-mythes-pt-pt \
-mythes-ru \
-onboard \
-pix \
-pix-dbg \
-python3-uno \
-redshift-gtk \
-rhythmbox \
-rhythmbox-data \
-rhythmbox-plugins \
-rhythmbox-plugin-tray-icon \
-seahorse \
-simple-scan \
-sticky \
-thunderbird \
-thunderbird-gnome-support \
-thunderbird-locale-de \
-thunderbird-locale-en \
-thunderbird-locale-en \
-thunderbird-locale-en-gb \
-thunderbird-locale-en-us \
-thunderbird-locale-en-us \
-thunderbird-locale-es \
-thunderbird-locale-es-ar \
-thunderbird-locale-es-es \
-thunderbird-locale-fr \
-thunderbird-locale-fr \
-thunderbird-locale-it \
-thunderbird-locale-pt \
-thunderbird-locale-pt-br \
-thunderbird-locale-pt-pt \
-thunderbird-locale-ru \
-thunderbird-locale-zh-cn \
-thunderbird-locale-zh-hans \
-thunderbird-locale-zh-hant \
-thunderbird-locale-zh-tw \
-transmission-gtk \
-uno-libs-private \
-ure \
-ure-java \
-warpinator \
-webapp-manager \
-xed \
-xed-dbg \
-xreader \
-xreader-dbg \
-xviewer \
-xviewer-dbg \
-xviewer-plugins \
-#
-echo "Nettoyage..."
-apt autoremove
+
+# ==============================================================================
+# TITRE: Création d'une installation minimale de Linux Mint Cinnamon 21
+# AUTEUR: Amaury Libert (Base) | Amélioré par l'IA
+# LICENCE: GPLv3
+# DESCRIPTION:
+#   Supprime un ensemble de paquets par défaut (LibreOffice, Thunderbird, Média,
+#   Jeux) et leurs dépendances pour alléger le système.
+# ==============================================================================
+
+# --- Configuration et Préparation ---
+
+# Mode strict: Quitte en cas d'erreur (-e), variable non définie (-u), ou échec
+# dans un pipe (-o pipefail). Indispensable pour les scripts sudo.
+set -euo pipefail
+
+# Couleurs pour une meilleure sortie
+VERT='\033[0;32m'
+ROUGE='\033[0;31m'
+JAUNE='\033[0;33m'
+CYAN='\033[0;36m'
+FIN='\033[0m'
+
+# Vérification des droits root
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e "${ROUGE}ERREUR : Ce script doit être exécuté avec 'sudo' ou en tant que root.${FIN}"
+    exit 1
+fi
+
+echo -e "${CYAN}*** Début de la suppression des paquets par défaut... ***${FIN}"
+clear # Effacer l'écran après le message d'introduction
+
+# --- Liste des Paquets Principaux à Purger (Dépendances inutiles retirées) ---
+
+# Nous listons uniquement les paquets principaux. La plupart des paquets 'hunspell',
+# 'hyphen', 'mythes', et de nombreuses librairies 'lib*' seront retirés
+# automatiquement et en toute sécurité par 'apt autoremove'.
+PAQUETS_PRINCIPAUX_A_PURGER=(
+    # --- Suite Bureautique & Email ---
+    thunderbird
+    libreoffice-writer
+    libreoffice-calc
+    libreoffice-impress
+    libreoffice-math
+    # Retirer les paquets de support GNOME/Java/Base pour purger la suite complète
+    libreoffice-base
+    libreoffice-base-core
+    libreoffice-base-drivers
+    libreoffice-core
+    libreoffice-draw
+    libreoffice-gnome
+    libreoffice-gtk3
+    libreoffice-java-common
+    liblibreoffice-java
+    libreoffice-sdbc-hsqldb
+    libreoffice-style-colibre
+    thunderbird-gnome-support
+
+    # --- Applications Média, Vidéo & Internet ---
+    celluloid      # Lecteur Vidéo
+    rhythmbox      # Lecteur Musique
+    hypnotix       # IPTV
+    transmission-gtk
+    hexchat        # IRC
+    warpinator     # Transfert de fichiers local
+    webapp-manager # Gestionnaire d'applications web
+
+    # --- Applications Graphiques et Utilitaires GNOME/Mint ---
+    baobab         # Analyseur d'utilisation de disque
+    drawing        # Outil de dessin simple
+    gnome-calculator
+    gnome-calendar
+    gnome-disk-utility # Utilitaire de disques
+    gnome-font-viewer
+    gnome-screenshot
+    gucharmap      # Carte des caractères
+    mintstick      # Créateur de clef USB
+    onboard        # Clavier virtuel
+    pix            # Visionneuse d'images (fork de Eye of GNOME)
+    redshift-gtk   # Ajustement de la température des couleurs
+    seahorse       # Gestionnaire de clés/mots de passe
+    simple-scan    # Scan
+    sticky         # Notes adhésives
+    # XApps (éditeurs de texte, visionneuses, lecteurs)
+    xed            # Éditeur de texte (fork de Gedit)
+    xreader        # Lecteur de documents (fork de Evince)
+    xviewer        # Visionneuse d'images
+
+    # --- Dépendances mineures (la plupart gérées par autoremove, mais listées si nécessaire) ---
+    bison          # Outil de développement
+    dc             # Calculatrice en ligne de commande
+    fonts-opensymbol # Police pour LibreOffice
+
+    # --- Paquets de débogage (dbg) qui seront probablement retirés par autoremove ---
+    pix-dbg
+    xed-dbg
+    xreader-dbg
+    xviewer-dbg
+)
+
+# --- Exécution de la Purge ---
+
+echo -e "${JAUNE}1. Purge des paquets principaux (${#PAQUETS_PRINCIPAUX_A_PURGER[@]} paquets)...${FIN}"
+
+# Exécuter la purge en utilisant xargs pour la longue liste
+printf '%s\n' "${PAQUETS_PRINCIPAUX_A_PURGER[@]}" | xargs apt purge --ignore-missing -y
+
+if [ $? -ne 0 ]; then
+    echo -e "${ROUGE}AVERTISSEMENT : La purge APT a rencontré des erreurs (paquets ignorés). Poursuite du nettoyage.${FIN}"
+fi
+echo -e "${VERT}Paquets principaux purgés avec succès.${FIN}"
+
+# --- Nettoyage Final (autoremove) ---
+
+echo -e "${JAUNE}2. Nettoyage final du système (suppression des dépendances inutiles)...${FIN}"
+
+# Utilisation de apt autoremove -y pour retirer toutes les dépendances devenues
+# inutiles suite à la purge ci-dessus (y compris les dictionnaires, locales, librairies, etc.).
+apt autoremove -y
+
+echo -e "${VERT}*** Nettoyage terminé. Votre installation de Linux Mint est maintenant minimale. ***${FIN}"
